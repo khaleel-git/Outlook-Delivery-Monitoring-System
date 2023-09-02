87368ebaa1f73c26fe6b7e60197b19ecc98edb7b
Khaleel A <khaleel.org@gmail.com>
2023-09-02 16:03:02 +0500

    delvery_monitoring with outlook

---
+++ README.md
@@ -0,0 +1,47 @@
+# Outlook Delivery Monitoring System
+
+The **Outlook Delivery Monitoring System** is a Python-based tool designed to track and monitor daily deliveries through Microsoft Outlook's Sent Items folder. This system is particularly useful for businesses and organizations that rely on email communication for critical deliveries and need to ensure the timely delivery of important information.
+
+## Table of Contents
+- [Overview](#overview)
+- [Key Features](#key-features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Acknowledgments](#acknowledgments)
+- [Contributing](#contributing)
+
+## Overview
+
+Email communication is a fundamental part of modern business operations. Ensuring that important messages, reports, and notifications are delivered promptly is crucial. The **Outlook Delivery Monitoring System** automates the process of monitoring deliveries and provides insights into the status of each delivery. It accomplishes this by comparing sent emails with expected deliveries and categorizing them based on their status.
+
+## Key Features
+
+- **Automated Monitoring:** The system automatically scans the Sent Items folder in Microsoft Outlook to identify sent emails.
+
+- **Status Categorization:** It categorizes emails based on their delivery status, including "Success," "Late Delivery," "Pending Delivery," and "Delivery Unsuccessful."
+
+- **Real-time Reporting:** Users receive real-time reports summarizing the delivery status of sent emails.
+
+- **Customizable Alerts:** The system can be configured to send alerts or notifications when certain conditions are met, such as late deliveries or delivery failures.
+
+- **Email Owners:** It associates each email with its respective owner or sender, providing insights into responsible parties.
+
+## Installation
+
+To get started with the **Outlook Delivery Monitoring System**, follow these steps:
+
+1. Clone this repository to your local machine.
+
+2. Install the required dependencies using the following command:
+
+```bash
+   pip install -r requirements.txt
+```
+
+## Run the project with:
+```
+python delivery_check.py
+```
+## Usage
+To extract and monitor deliveries from a specific time frame, the system uses the Sent Items folder in Microsoft Outlook. It compares the sent emails with a list of expected deliveries and categorizes them based on their delivery status.
---
+++ delivery_check.py
@@ -0,0 +1,145 @@
+import random
+import win32com.client
+import datetime
+import pymssql
+
+today = datetime.date.today()
+
+'''
+Function: Send_Email()
+Arguments: 
+	table_subjects, email_times, table_expected_times, email_status
+'''
+def Send_Email(table_subjects, email_times, table_expected_times, email_status, table_owners):
+	# Sort all lists based on email_times
+	# sorted_lists = sorted(zip(email_times, table_subjects, table_expected_times, email_status, table_owners))
+	sorted_lists = sorted(zip(table_expected_times, table_subjects, email_times, email_status, table_owners))
+
+
+	html_table = "<table>\n"
+	html_table += "<tr><th><h3>Email Subject</h3></th><th><h3>Email Time</h3></th><th><h3>Expected Time</h3></th><th><h3>Email Status</h3></th><th><h3>Owner</h3></th></tr>\n"
+
+	# for time, subject, expected_time, status, owner in sorted_lists:
+	for expected_time,subject,time,status,owner  in sorted_lists:
+		if status == "Delivery Unsuccessful":
+			html_table += '<tr class="warning">'
+		elif status == "Success":
+			html_table += '<tr class="success">'
+		elif status == "Late Delivery":
+			html_table += '<tr class="success">'
+		else:
+			html_table += "<tr>"
+			
+		html_table += f"<td>{subject}</td>"
+		html_table += f"<td>{time}</td>"
+		html_table += f"<td>{expected_time}</td>"
+		html_table += f"<td>{status}</td>"
+		html_table += f"<td>{owner}</td>"
+		html_table += "</tr>\n"
+
+	html_table += "</table>"
+
+	with open('E:\\Khaleel\\EricMon\\EricMon.css', 'r') as css_file:
+		css_content = css_file.read()
+
+	html_head = f"<head><style>{css_content}</style></head>"
+
+	header = f"<h1>Delivery Monitoring</h1>"
+	boday =  f'''
+					<h2>Delivery Status So far, Time: {datetime.datetime.now().strftime("%H:%M")}</h2>
+			  '''
+
+	footer = "<p><font size=""2"" face=""Bahnschrift Light Condensed"" color=""Gray""><br>This is a system generated email, please report anomalies to  <a href=""mailto:ENPMPERFORMANCETEAM@jazz.com.pk"">ENPMPERFORMANCETEAM@jazz.com.pk</a></p></html>"
+
+	HTMLBody = html_head + header + boday + html_table + footer
+
+
+	obj = win32com.client.Dispatch("Outlook.Application")
+	outlook_obj = obj.CreateItem(0)
+	outlook_obj.SentOnBehalfOfName = ''
+	# outlook_obj.To = "khaleel Ahmad <khaleel.org@gmail.com>; <khalil.a.ahmed@ericsson.com>"
+	outlook_obj.To = ""
+	outlook_obj.Subject = f"EricMon Delivery Monitoring | " + today.strftime('%d-%m-%Y')
+	outlook_obj.HTMLBody = HTMLBody
+	outlook_obj.Send()
+
+'''
+End of Function: Send_Email
+'''
+
+if __name__ == '__main__':
+	# Get delivey Info from EricMon Database -> Table: Meta_Delivery
+	conn = pymssql.connect(host="", user="", password="", database="EricMon")
+	cur = conn.cursor()
+	query = "SELECT * FROM [EricMon].[dbo].[Meta_Delivery]"
+	cur.execute(query)
+	rows = cur.fetchall()
+
+	# Outlook configurations
+	outlook_app = win32com.client.Dispatch("Outlook.Application")
+	namespace = outlook_app.GetNamespace("MAPI")
+	sent_folder = namespace.GetDefaultFolder(5)  # 5 represents the index of the sent folder
+
+	# Filter only today's Sent Emails
+	filter_str = f"[SentOn] >= '{today.strftime('%m/%d/%Y')} 00:00 AM' AND [SentOn] <= '{today.strftime('%m/%d/%Y')} 11:59 PM'"
+	sent_emails = sent_folder.Items.Restrict(filter_str)  # Apply the filter
+
+	# Initialization
+	table_subjects = []
+	table_expected_times = []
+	table_owners = []
+
+	email_times = []
+	email_status = []
+
+	bot_counts, repeat_counts = 0, 0
+
+	for row in rows:
+		table_subject = row[2].rsplit("|", 1)[0].strip()
+		expected_delivery_time = row[3]
+
+		table_subjects.append(table_subject)
+		table_owners.append(row[5])
+
+		email_found = False  # Flag to check if an email was found for the subject
+		for email in sent_emails:
+			email_subject = email.Subject.rsplit("|", 1)[0].strip()
+
+			if "::" in email.Subject:
+				email_subject = email.Subject.rsplit("::", 1)[0].strip()
+
+			if "||" in email.Subject:
+				email_subject = email.Subject.rsplit("||", 1)[0].strip()
+
+			if email_subject.lower() == table_subject.lower():
+				email_times.append(email.SentOn.strftime("%H:%M"))
+				table_expected_times.append(expected_delivery_time)
+				if expected_delivery_time < email.SentOn.strftime("%H:%M"):
+					email_status.append('Late Delivery')
+				else:
+					email_status.append('Success')
+				email_found = True
+				break  # Found the email, exit the loop
+
+		if not email_found:
+			current_time = datetime.datetime.now().strftime("%H:%M")
+			if current_time > expected_delivery_time:
+				email_times.append(' ')
+				email_status.append('Delivery Unsuccessful')
+			else:
+				email_times.append(' ')
+				email_status.append('Pending Delivery')
+			table_expected_times.append(expected_delivery_time)
+
+	# Execute Send_Email Function
+	Send_Email(table_subjects, email_times, table_expected_times, email_status, table_owners)
+
+	# Print the counts of the lists
+	print(f"Email Subjects, length: {len(table_subjects)}")
+	print(f"Email Times, length: {len(email_times)}")
+	print(f"Email Statuses, length: {len(email_status)}")
+	print(f"Owners Count, length: {len(table_owners)}")
+
+	# Release resources from RAM
+	namespace = None
+	outlook_app = None
